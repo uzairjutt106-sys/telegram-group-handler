594dd4384aa89e0954563f3d48f084ec3eec8e8b
uzairjutt106 <uzairjutt106@gmail.com>
2025-09-03 15:16:00 +0000

    Initial project upload

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 import os
 
-from flask import Flask, send_file
 
-app = Flask(__name__)
+BOT_TOKEN = "8027538894:AAHM68ckyxHN7QPKCPOLrscn_BTM5j6avCI"
 
-@app.route("/")
-def index():
-    return send_file('src/index.html')
+
+
+async def check_message(update: Update, context:ContextTypes.DEFAULT_TYPE):
+    message = update.message.text
+    user = update.message.from_user.first_name  
+    await update.message.reply_text("Hello! I am alive 🚀")
+    if "http://" in message or "https://" in message or "www." in message:
+        # Delete the message
+        await update.message.delete()
+        print(f"Deleted a link message from {user}")
+
+
+
+
+
+
+
 
 def main():
-    app.run(port=int(os.environ.get('PORT', 80)))
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+
+    # Register /start command
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, check_message))
+
+    # Run bot
+    print("bot is runing")
+    app.run_polling()
 
 if __name__ == "__main__":
     main()
